1472928c29e6977a3d8ed42414374e42e486ac35
thismax <max.g.jacobs@gmail.com>
2017-10-12 19:52:43 -0700

    minor refactor

--- db/db.js
+++
@@ -1,9 +0,0 @@
-let url = require('./config').url;
-let mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
-mongoose.connect(url)
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-});
