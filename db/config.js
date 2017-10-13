4965361a8c801a6c35ded3fa423dc5961fd4fb95
thismax <max.g.jacobs@gmail.com>
2017-10-13 16:08:45 -0700

    added db schema

--- db/config.js
+++ db/config.js
@@ -1,13 +1,78 @@
-let url = require('./url').url;
-let mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+const Sequelize = require('sequelize');
+const url = require('./url')
+//this initializes the database.
+const DB = new Sequelize(url);
+//This initializes and authenticates the database
+DB.authenticate()
+.then(() => {
+  console.log('Connection to database has been established');
+})
+.catch((err) => {
+  console.error('Unable to connect to the database', err);
+});
 
-const db = mongoose.createConnection(url, { config: {autoIndex: false} }, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('connected to db')
+const User = DB.define('user', {
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  sandbox1Id: {
+    type: Sequelize.INTEGER,
+    model: 'sandbox',
+    key: 'id',
+  },
+  sandbox2Id: {
+    type: Sequelize.INTEGER,
+    model: 'sandbox',
+    key: 'id',
+  },
+  sandbox3Id: {
+    type: Sequelize.INTEGER,
+    model: 'sandbox',
+    key: 'id',
+  },
+});
+
+const Sandbox = DB.define('sandbox', {
+  name: {
+    type: Sequelize.STRING,
+  },
+  model: {
+    type: Sequelize.INTEGER,
+    model: 'model',
+    key: 'id',
+  },
+  data: {
+    type: Sequelize.STRING,
   }
 });
 
-module.exports = { db }
+const Model = DB.define('model', {
+  type: {
+    type: Sequelize.STRING,
+  },
+  params: {
+    type: Sequelize.STRING,
+  }
+})
+
+User.hasMany(Sandbox, { onDelete: 'cascade' });
+Sandbox.belongsTo(User, { onDelete: 'cascade' });
+Model.belongsTo(Sandbox, { onDelete: 'cascade' });
+
+User.sync().then(() => {
+  Sandbox.sync().then(() => {
+    Model.sync();
+  });
+});
+
+module.exports = {
+  DB,
+  User,
+  Sandbox,
+  Model,
+};
