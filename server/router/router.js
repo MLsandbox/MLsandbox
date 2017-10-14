5f16502a3e6d44517b8a947037dd82afe34802ef
thismax <max.g.jacobs@gmail.com>
2017-10-14 12:23:51 -0700

    setting up routes

--- server/router/router.js
+++ server/router/router.js
@@ -1,31 +1,37 @@
 const path = require('path');
 const url = require('url');
-const controller = require('../controller/controller.js');
+const utils = require('../utils/http-helpers.js');
+const DB = require('../../db/config.js');
 
 const actions = {
-  GET: (req, res) => {
-    /* if your router needs to pattern-match endpoints
-     * var parsedUrl = url.parse(req.url);
-     * var endPoint = parsedUrl.pathname === '/' ? '/index.html' : parsedUrl.pathname;
-    */
-    /*
-     * DO SOMETHING - get asset, query database, etc. -> store as `data`
-     * pass the result of that operation as data into responder -> store result status code as `statusCode`
-     * pass the status code into responder
-    */
-    controller.respond(res, data, statusCode);
-  },
 
   POST: (req, res) => {
-    utils.prepareResponse(req, function(data) {
-      // Do something with the data that was just collected by the helper
-      // e.g., validate and save to db
-      // either redirect or respond
-        // should be based on result of the operation performed in response to the POST request intent
-        // e.g., if user wants to save, and save fails, throw error
-      utils.redirector(res, /* redirect path , optional status code -  defaults to 302 */);  
+    
+    let parsedUrl = url.parse(req.url);
+    let endPoint = parsedUrl.pathname === '/' ? '/index.html' : parsedUrl.pathname;
+
+    let data = utils.prepareResponse(req, (data) => {
+      
+      console.log('====================================')
+      console.log(data.username);
+      console.log('====================================')
+
+      DB.User.create({
+        username: data.username,
+        password: data.password,
+        sandbox1: null,
+        sandbox2: null,
+        sandbox3: null,
+      }).then((newUser) => {
+        utils.respond(res, newUser, statusCode);
+      }).catch((err) => {
+        utils.send404(res);
+      });
+      
     });
-  }
+
+  },
+
 }; 
 
 exports.handleRequest = function(req, res) {
