0eb24ace0c087b280aba6fde049f87a1bde3e120
thismax <max.g.jacobs@gmail.com>
2017-10-14 11:29:39 -0700

    refactored server

--- server/index.js
+++ server/index.js
@@ -1,25 +1,13 @@
 let http = require('http');
-let fs = require('fs');
-let path = require('path');
+let router = require('./routes/routes.js');
 
-let headers = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10, // Seconds.
-  'Content-Type': 'text/html'
-};
+let server = http.createServer(router.handleRequest);
 
-http.createServer((req, res) => {
-  fs.readFile(path.resolve(__dirname, '../client/static/index.html'), 'utf8', (err, data) => {
-    if (err) console.log(err);
-    res.writeHead(200, headers);
-    res.write(data);
-    res.end();
-  });
-})
-.listen(1337, '127.0.0.1', ()=> {
+let port = 1337;
+let ip = '127.0.0.1';
+
+server.listen(port, ip, ()=> {
   console.log('====================================');
-  console.log('server is listening on port 1337');
+  console.log("Listening on http://" + ip + ":" + port);
   console.log('====================================');
 }); 
