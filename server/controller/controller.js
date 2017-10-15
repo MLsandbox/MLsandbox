a8100425ea7160d7f403648f802a8ed22d43e112
thismax <max.g.jacobs@gmail.com>
2017-10-14 17:08:24 -0700

    added controller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,4 +1,42 @@
-// DB = require('../../db/config.js');
+DB = require('../../db/config.js');
+utils = require('../utils/http-helpers');
+
+module.exports.signUp = (req, res) => {
+  utils.prepareResponse(req, (data) => {    
+    let username = JSON.parse(data).username;
+    let password = JSON.parse(data).password;
+    DB.User.findOne({where: {username}})
+    .then((result) => {
+      if (!result) {
+        DB.User.create({
+          username,
+          password,
+        }).then((newUser) => {
+          res.end('new user created!');
+        }).catch((err) => {
+          utils.send404(res);
+        });
+      }
+      else {
+        res.end('that user already exists!'); // needs to redirect to login
+      }
+    })
+    .catch((err) => {
+      console.log('====================================')
+      console.log('nah')
+      console.log('====================================')
+      res.end()
+    })
+    // DB.User.create({
+    //   username,
+    //   password,
+    // }).then((newUser) => {
+    //   res.end('new user created!');
+    // }).catch((err) => {
+    //   utils.send404(res);
+    // });
+  })
+};
 
 // fs.readFile(path.resolve(__dirname, '../client/static/index.html'), 'utf8', (err, data) => {
 //   if (err) console.log(err);
@@ -8,16 +46,4 @@
 // });
 
 
-// module.exports.addUser = (req, res) => {
-//   DB.User.create({
-//     username: req.body.username,
-//     password: req.body.password,
-//     sandbox1: null,
-//     sandbox2: null,
-//     sandbox3: null,
-//   }).then((newUser) => {
-//     res.status(201).json(newUser);
-//   }).catch((err) => {
-//     res.status(404).json(err);
-//   });
-// };
+ 
--- server/router/router.js
+++ server/router/router.js
@@ -2,35 +2,20 @@ const path = require('path');
 const url = require('url');
 const utils = require('../utils/http-helpers.js');
 const DB = require('../../db/config.js');
+const controller = require('../controller/controller.js');
 
 const actions = {
 
   POST: (req, res) => {
-    
-    let parsedUrl = url.parse(req.url);
-    let endPoint = parsedUrl.pathname === '/' ? '/index.html' : parsedUrl.pathname;
-
-    let data = utils.prepareResponse(req, (data) => {
-      
-      console.log('====================================')
-      console.log(data.username);
-      console.log('====================================')
-
-      DB.User.create({
-        username: data.username,
-        password: data.password,
-        sandbox1: null,
-        sandbox2: null,
-        sandbox3: null,
-      }).then((newUser) => {
-        utils.respond(res, newUser, statusCode);
-      }).catch((err) => {
-        utils.send404(res);
-      });
-      
-    });
-
+    let endPoint = url.parse(req.url).pathname;
+    if (endPoint === '/api/signUp') {
+      controller.signUp(req, res);
+    }
   },
+
+  GET: (req, res) => {
+    let parsedUrl = url.parse
+  }
 
 }; 
 
