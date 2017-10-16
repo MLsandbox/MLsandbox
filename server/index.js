e5df9ef4700890cf730f27028d7b061db9f32805
thismax <max.g.jacobs@gmail.com>
2017-10-16 11:20:34 -0700

    refactored into express

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
-let http = require('http');
-let router = require('./router/router.js');
-
-let server = http.createServer(router.handleRequest);
-
-let port = 1337;
-let ip = '127.0.0.1';
-
-server.listen(port, ip, ()=> {
-  console.log('====================================');
-  console.log("Listening on http://" + ip + ":" + port);
-  console.log('====================================');
-}); 
+// dependencies
+const express = require('express');
+const parser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+// file imports
+// const router = require('./router/router.js');
+const db = require('../db/config.js');
+// express instantiation
+const app = express();
+const port = process.env.PORT || 8080;
+// middleware
+app.use(parser.json());
+app.use(parser.urlencoded());
+app.use(cors());
+// app.use('/api', router);
+app.use(express.static(__dirname + '../client/static/index.html'));
+// sync db, start a UNIX socket and listen for connections
+db.User.sync({force: true}).then(() => app.listen(port, () => console.log("Listening on port " + port)));
