7bb2b2c773ce6c7ac6f92cc5ca6d31d64961bf45
thismax <max.g.jacobs@gmail.com>
2017-10-16 11:42:48 -0700

    refactored signUp controller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,50 +1,27 @@
 DB = require('../../db/config.js');
-utils = require('../utils/http-helpers');
 
 module.exports.signUp = (req, res) => {
-  utils.prepareResponse(req, (data) => {    
-    let username = JSON.parse(data).username;
-    let password = JSON.parse(data).password;
-    DB.User.findOne({where: {username}})
-    .then((result) => {
-      if (!result) {
-        DB.User.create({
-          username,
-          password,
-        }).then((newUser) => {
-          res.end('new user created!');
-        }).catch((err) => {
-          utils.send404(res);
-        });
-      }
-      else {
-        res.end('that user already exists!'); // needs to redirect to login
-        // utils.redirector(res, '/login');
-      }
-    })
-    .catch((err) => {
-      console.log('====================================')
-      console.log('nah')
-      console.log('====================================')
-      res.end()
-    })
-    // DB.User.create({
-    //   username,
-    //   password,
-    // }).then((newUser) => {
-    //   res.end('new user created!');
-    // }).catch((err) => {
-    //   utils.send404(res);
-    // });
+  DB.User.findOne({where: {
+    username: req.body.username,
+  }})
+  .then((result) => {
+    if (!result) {
+      DB.User.create({
+        username: req.body.username,
+        password: req.body.password,
+      }).then((newUser) => {
+        res.status(201).send(newUser)
+      }).catch((err) => {
+        res.status(404).send(err);
+      });
+    }
+    else {
+      res.end('that user already exists!');
+    }
+  })
+  .catch((err) => {
+    res.status(404).send(err);
   })
 };
-
-// fs.readFile(path.resolve(__dirname, '../client/static/index.html'), 'utf8', (err, data) => {
-//   if (err) console.log(err);
-//   res.writeHead(200, headers);
-//   res.write(data);
-//   res.end();
-// });
-
 
  
