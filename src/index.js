cde3872ed2904454a535e6f403f4a4af0e0e14c4
Unknown <jnguy142@ucr.edu>
2017-10-18 11:39:41 -0700

    front end skeleton + some cs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, browserHistory} from 'react-router-dom';
+import Menu from './Menu/index';
 import store from './Redux/store';
 import App from './App/App';
 
 
 render(
 <Provider store={store}>
-  <App />
+  <BrowserRouter history={browserHistory}>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path ="/menu" component={Menu} />
+    </div>
+  </BrowserRouter>
 </Provider>, document.getElementById('app'));
