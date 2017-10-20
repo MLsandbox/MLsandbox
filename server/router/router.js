485f591d364a3f04b540dbd7ce2738a51aa0397d
thismax <max.g.jacobs@gmail.com>
2017-10-20 15:20:30 -0700

    added delete user / change pass

--- server/router/router.js
+++ server/router/router.js
@@ -6,5 +6,7 @@ const controller = require('../controller/controller.js');
 router.post('/signUp', controller.signUp);
 router.post('/logIn', controller.logIn);
 router.post('/logOut', controller.logOut);
+router.delete('/deleteAccount', controller.deleteAccount);
+router.post('/changePassword', controller.changePassword);
 // export router
 module.exports = router;
