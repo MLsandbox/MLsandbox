7e08272c7e43dd47bc2eec9e0344acdd91a8a547
Ubuntu <ubuntu@ip-172-31-34-8.us-east-2.compute.internal>
2017-10-27 23:28:31 +0000

    resolved more https issues

--- src/Sandbox/Options.js
+++ src/Sandbox/Options.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-var placeholder = "http://3.bp.blogspot.com/-JfemdXlQ-Ns/UzHKRp3KoeI/AAAAAAABAmY/7N2ghKTYI9o/s3200/Screen+Shot+2014-03-25+at+7.21.15+PM.png";
+
 var Options = () => {
   return (
     <div>
@@ -14,9 +14,6 @@ var Options = () => {
       </ul>
 
       <h1>Placeholder SandBox</h1>
-      <div>
-        <img src={placeholder} height="300" width="300"/>
-      </div>
     </div>
   );
 }
