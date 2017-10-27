788d8d23e10e739243f1bee33a2203e0121ea083
thismax <max.g.jacobs@gmail.com>
2017-10-27 16:46:02 -0700

    implemented gzip

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const { resolve } = require('path');
 const srcDir = resolve(__dirname, 'src');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+var CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: `${srcDir}/index.js`,
@@ -59,5 +60,12 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new webpack.optimize.ModuleConcatenationPlugin(),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
   ]
 }
