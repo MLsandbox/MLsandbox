35822de27cf9a4d17bccb5d49f83191f580d5739
Unknown <jnguy142@ucr.edu>
2017-10-27 10:56:08 -0700

    optimization of production bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const { resolve } = require('path');
 const srcDir = resolve(__dirname, 'src');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -39,6 +37,26 @@ module.exports = {
       $: "jquery",
       jQuery: "jquery"
     }),
-    new UglifyJSPlugin(), 
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
+      }
+    }), 
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
   ]
 }
