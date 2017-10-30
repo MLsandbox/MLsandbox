a977cf9799bc5114248ac4d67aae3a016fad5b4a
thismax <max.g.jacobs@gmail.com>
2017-10-30 13:14:39 -0700

    made better drawer

--- src/Sandbox/Drawer.js
+++ src/Sandbox/Drawer.js
@@ -1,76 +1,30 @@
 import React from 'react';
-import ReactDrawer from 'react-drawer';
-import 'react-drawer/lib/react-drawer.css';
+import { Link } from 'react-router-dom';
  
-class Drawer extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      open: false,
-      position: 'right',
-      noOverlay: false
-    };
-  }
-
-
-  setPosition = (e) => {
-    this.setState({position: e.target.value});
-  }
-  setNoOverlay = (e) => {
-    this.setState({noOverlay: e.target.checked});
-  }
-  toggleDrawer = () => {
-    this.setState({open: !this.state.open});
-  }
-  closeDrawer = () => {
-    this.setState({open: false});
-  }
-  onDrawerClose = () => {
-    this.setState({open: false});
-  }
-
-
-  render() {
-    return (
-      <div>
-        <div style={{margin: 200}}>
-          <h1>React Drawer configuration</h1>
-          <div style={{margin: 20}}>
-            <label>Position</label>
-            <select value={this.state.position} onChange={this.setPosition}>
-              <option value="top">top</option>
-              <option value="bottom">bottom</option>
-              <option value="left">left</option>
-              <option value="right">right</option>
-            </select>
-          </div>
-          <div style={{margin: 20}}>
-            <input type="checkbox"
-              checked={this.state.noOverlay}
-              onChange={this.setNoOverlay}
-            />
-            <label>No overlay</label>
-            <small>(The overlay lets you close the drawer on click)</small>
-          </div>
-          <button
-            style={{margin: 20}}
-            onClick={this.toggleDrawer}
-            disabled={this.state.open && !this.state.noOverlay}
-            >
-            {!this.state.open ? <span>show drawer</span>: <span>close drawer</span>}
-          </button>
-        </div>
-        <ReactDrawer
-          open={this.state.open}
-          position={this.state.position}
-          onClose={this.onDrawerClose}
-          noOverlay={this.state.noOverlay}>
-          <i onClick={this.closeDrawer} className="icono-cross"></i>
-          <h2>What a nice drawer !</h2>
-        </ReactDrawer>
+const Drawer = (props) => {
+  return (
+    <nav className="navbar navbar-dark drawer">
+      <a className="navbar-brand transparent" href="#">ml-sandbox</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#siteNav"
+        aria-controls="siteNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="siteNav">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to ="/sandbox">BACK</Link>
+          </li>
+        </ul>
       </div>
-    );
-  }
+    </nav>
+  )
 }
 
 export default Drawer;
