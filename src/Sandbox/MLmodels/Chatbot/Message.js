4b839657fda18de0bdccd98a6446d1fcec3e4381
thismax <max.g.jacobs@gmail.com>
2017-10-31 12:46:33 -0700

    aligned responses

--- src/Sandbox/MLmodels/Chatbot/Message.js
+++ src/Sandbox/MLmodels/Chatbot/Message.js
@@ -2,15 +2,10 @@ import React, { Component } from 'react';
 
 const Message = ({message}) => {
   if (message.message) {
-    if (true) {
-      return (
-        <li className="sender"><div className="sender-bubble">{message.message}</div></li> 
-      );
-    } else {
-      return (
-        <li className="recipient"><div className="recipient-bubble">{message.message}</div></li> 
-      );
-    }
+    console.log(message);
+    return (
+      <li className={`${message.from}-message`}><div className="sender-bubble">{message.message}</div></li> 
+    );
   } else {
     return null;
   }
