733a89ddfc9300094aba0d09a77d359f4a7aa937
Unknown <jnguy142@ucr.edu>
2017-11-01 12:21:35 -0700

    restyled dashboard to match theme

--- src/Sandbox/Dashboard/index.js
+++ src/Sandbox/Dashboard/index.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 
 var Dashboard = (props) => {
   return(
-    <div className='wrapper'>
-      <input type='checkbox'></input>
-      <label>
-        <div className='circle'></div>
-        <Link to ={props.link}>{props.text}</Link>
-      </label>
-    </div>
+    <Link to ={props.link}>
+      <div className='wrapper'>
+        <input type='checkbox'></input>
+        <label>
+          <div className='circle'></div>
+          {props.text}
+        </label>
+      </div>
+    </Link>
   );
 }
 
