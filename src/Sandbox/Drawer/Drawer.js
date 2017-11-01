714fcbdaaad84bfdfdd19c53a614154166ba8493
thismax <max.g.jacobs@gmail.com>
2017-11-01 10:58:38 -0700

    added modal

--- src/Sandbox/Drawer/Drawer.js
+++ src/Sandbox/Drawer/Drawer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logout from '../../Logout/index.js';
 import './drawerStyles.css'
  
 const Drawer = ({modelName}) => {
@@ -22,10 +23,26 @@ const Drawer = ({modelName}) => {
           <li className="nav-item">
             <Link className="nav-link" to ="/sandbox">BACK</Link>
           </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#" data-toggle="modal" data-target="#explanation-popup" >EXPLANATION</a>
+            <div id="explanation-popup" className="modal fade" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLiveLabel" style={{display: "none"}} aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-body">
+                    HIIII
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li className="nav-item">
+            <Logout className="nav-link"/> 
+          </li>
         </ul>
       </div>
     </nav>
   )
 }
+
 
 export default Drawer;
