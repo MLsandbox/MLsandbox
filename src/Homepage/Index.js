104d734309817c177dc47459364f9e27ee6a46b7
Unknown <jnguy142@ucr.edu>
2017-11-01 09:20:41 -0700

    styled new refactor modal

--- src/Homepage/Index.js
+++ src/Homepage/Index.js
@@ -10,17 +10,7 @@ import Styles from'./index.css';
 class Homepage extends Component {
   constructor (props) {
     super(props);
-    this.openPopup = this.openPopup.bind(this);
-    this.closePopup = this.closePopup.bind(this);
     this.signIn = this.signIn.bind(this);
-  }
-
-  closePopup () {
-    this.props.dispatch(setPopupState());
-  }
-
-  openPopup () {
-    this.props.dispatch(setPopupState());
   }
 
   signIn() {
@@ -39,20 +29,15 @@ class Homepage extends Component {
       <div id="homepage-root">
         <Login 
         processing = {this.props.processing}
-        signIn = {this.signIn} 
-        closePopup={this.closePopup} 
-        popupState={popupState}/>
+        signIn = {this.signIn} />
         <section className="intro">
           <div className="inner">
             <div className="content">
                 <h1>Welcome</h1>
-                <a id="login-btn" href="#" onClick={this.openPopup} >LOGIN</a>
+                <a id="login-btn" href="#" data-toggle="modal" data-target="#auth-popup" >LOGIN</a>
             </div>
           </div>
         </section>
-        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalLive">
-          Launch demo modal
-        </button>
         <Footer />
         <h1><Link to ="/sandbox">Sandbox</Link></h1>
       </div>
@@ -62,7 +47,6 @@ class Homepage extends Component {
 
 var Homepagecomp = connect((store) => {
   return {
-    popupState:store.login.loginPopup,
     processing:store.auth.authentication.processing,
   }
 })(Homepage)
--- src/Homepage/Login/Login.js
+++ src/Homepage/Login/Login.js
@@ -6,9 +6,7 @@ var conditionalRender = (authState, props) => {
     return <Loader/>
   } else {
     return <Loginform 
-      signIn={props.signIn} 
-      signUp={props.signUp}
-      closePopup={props.closePopup}/>
+      signIn={props.signIn} />
   }
 }
 
@@ -21,21 +19,13 @@ var conditionalRender = (authState, props) => {
 
 var Login = (props) => {
   return (
-  <div id="exampleModalLive" className="modal fade" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLiveLabel" style={{display: "none"}} aria-hidden="true">
+  <div id="auth-popup" className="modal fade" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLiveLabel" style={{display: "none"}} aria-hidden="true">
     <div className="modal-dialog" role="document">
       <div className="modal-content">
-        <div className="modal-header">
-          <h5 className="modal-title" id="exampleModalLiveLabel">Modal title</h5>
-          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
         <div className="modal-body">
-          <p>Woohoo, you're reading this text in a modal!</p>
-        </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" className="btn btn-primary">Save changes</button>
+        <Loginform 
+          signIn={props.signIn} 
+        />
         </div>
       </div>
     </div>
