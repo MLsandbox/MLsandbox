a8c6dd5dc14c1d2a58f49ea847b217f09fb9e141
Unknown <jnguy142@ucr.edu>
2017-11-01 17:06:38 -0700

    can switch between login and signup form

--- src/Homepage/Login/Login.js
+++ src/Homepage/Login/Login.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Loginform from './Loginform';
+import Signupform from './Signupform';
 import Loader from './Loader';
+
 var conditionalRender = (authState, props) => {
   if (authState) {
     return <Loader/>
@@ -10,21 +12,54 @@ var conditionalRender = (authState, props) => {
   }
 }
 
-var Login = (props) => {
-  return (
-  <div id="auth-popup" className="modal fade" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLiveLabel" style={{display: "none"}} aria-hidden="true">
-    <div className="modal-dialog" role="document">
-      <div className="modal-content">
-        <div className="modal-body">
-          <Loginform 
-            signIn={props.signIn} 
-          />
+// <div className="modal-container">
+// <modal show={props.popupState} onHide={props.closePopup}>
+//   <div>
+//     {conditionalRender(props.authenticating, props)}
+//   </div>
+// </modal>
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { formtype: 'login'};
+    this.switchForm = this.switchForm.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+  }
+
+  switchForm() {
+    if ( this.state.formtype === 'login') {
+      this.setState({formtype: 'signup'});
+    }
+    if ( this.state.formtype === 'signup') {
+      this.setState({formtype: 'login'});
+    }
+  }
+
+  renderForm() {
+    if ( this.state.formtype === 'login') {
+      return (<Loginform switchForm={this.switchForm} signIn={this.props.signIn}/>);
+    }
+    if ( this.state.formtype === 'signup') {
+      return (<Signupform switchForm={this.switchForm} signIn={this.props.signIn}/>);
+    }
+  }
+
+  render(){
+    return (
+      <div id="auth-popup" className="modal fade" tabIndex="-1" role="dialog" 
+      aria-labelledby="exampleModalLiveLabel" style={{display: "none"}} aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-body">
+              { this.renderForm() }
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-
-  );
+    
+    );
+  }
 }
 
 export default Login;
