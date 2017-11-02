65755dd966b413e2840537178dec0a9caab77c94
Unknown <jnguy142@ucr.edu>
2017-11-02 08:24:47 -0700

    reducer logic for setting signup error state works

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import login from "./loginReducer";
 import auth from "./authReducer";
+import signup from './signupReducer';
 
 export default combineReducers({
   login,
