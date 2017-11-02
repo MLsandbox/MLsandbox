fda7b2753f3e6a41dac285c8665a01ee42cd1d7f
Unknown <jnguy142@ucr.edu>
2017-11-01 17:31:30 -0700

    updated actions to handle incorrect pw usr display

--- src/Redux/Reducers/loginReducer.js
+++ src/Redux/Reducers/loginReducer.js
@@ -2,7 +2,7 @@ import initState from '../initState';
 
 function loginReducer (state = initState, action) {
   switch (action.type) {
-    case 'SET_POPUP_STATE':
+    case 'SET_ERROR_STATE':
       return Object.assign({}, state, { loginPopup: !state.loginPopup });
     default:
       return state;
